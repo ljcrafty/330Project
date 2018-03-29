b6986a65cfae9c867d429186deb18ba7cb13fe08
Lauren <ljcrafty@comcast.net>
2018-03-29 21:53:08 +0200

    Start of Data and Data Access
    Created the initial book model and added my database access and exception classes

---
+++ Book.java
@@ -0,0 +1,101 @@
+public class Book
+{
+    private String title, author_fname, author_lname, genre_name, genre_desc;
+    private int num_copies;
+    private Calendar release_date;
+
+    public Book()
+    {
+        this.title          = "";
+        this.author_fname   = "";
+        this.author_lname   = "";
+        this.genre_name     = "";
+        this.genre_desc     = "";
+        this.num_copies     = 0;
+        this.release_date   = Calendar.getInstance();
+    }
+
+    public Book( String title, String author_fname, String author_lname, String genre_name, 
+        String genre_desc, int num_copies, Calendar release_date )
+    {
+        this.title = title;
+        this.author_fname = author_fname;
+        this.author_lname = author_lname;
+        this.genre_name = genre_name;
+        this.genre_desc = genre_desc;
+        this.num_copies = num_copies;
+        this.release_date = release_date;
+    }
+
+    //getters
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getAuthorFName()
+    {
+        return this.author_fname;
+    }        
+    
+    public String getAuthorLName()
+    {
+        return this.author_lname;
+    }
+                
+    public String getGenreName()
+    {
+        return this.genre_name;
+    }
+                
+    public String getGenreDesc()
+    {
+        return this.genre_desc;
+    }
+
+    public int getNumCopies()
+    {
+        return this.num_copies;
+    }
+
+    public Calendar getReleaseDate()
+    {
+        return this.release_date;
+    }
+
+    //setters
+    public void getTitle( String title )
+    {
+        this.title = title;
+    }
+
+    public void getAuthorFName( String name )
+    {
+        this.author_fname = name;
+    }        
+    
+    public void getAuthorLName( String name )
+    {
+        this.author_lname = name;
+    }
+                
+    public void getGenreName( String name )
+    {
+        this.genre_name = name;
+    }
+                
+    public void getGenreDesc( String desc )
+    {
+        this.genre_desc = desc;
+    }
+
+    public void getNumCopies( int num )
+    {
+        this.num_copies = num;
+    }
+
+    public void getReleaseDate( Calendar date )
+    {
+        this.release_date = date;
+    }
+}
