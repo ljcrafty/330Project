039b181a58d2d67cbd0de9fd1a7919c4ea9a6ed6
Lauren <ljcrafty@comcast.net>
2018-04-11 14:29:09 +0200

    I goofed
    I didn't even see the View file, so I moved the stuff I made into that file because that's where it belongs

--- Start.java
+++ Start.java
@@ -1,30 +1,7 @@
-import javax.swing.*;
-import java.awt.*;
-
 public class Start
 {
    public static void main(String args[])
    {
-      JFrame frame = new JFrame();
-      JPanel jp = new JPanel();
-      LoginView lv = new LoginView();
-      MainView libMain = new MainView("librarian");
-      MainView usrMain = new MainView("user");
-      RegisterView rv = new RegisterView();
-      
-      //configure card layout
-      CardLayout cards = new CardLayout(5, 5);      
-      jp.setLayout(cards);
-      jp.add(lv, "login");
-      jp.add(libMain, "librarianMain");
-      jp.add(usrMain, "userMain");
-      jp.add(rv, "register");
-      
-      cards.show(jp, "login");
-      frame.setContentPane(jp);
-      frame.setLocation(500, 125);
-      frame.setSize(600, 500);
-      frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-      frame.setVisible(true);
+      View view = new View();
    }
 }
